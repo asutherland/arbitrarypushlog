3eec178f250759e72b8f5298b662203ede140123
Andrew Sutherland <asutherland@asutherland.org>
2011-02-15 18:09:09 -0800

    Migrate from previous repo; asyncish hello world works with r.js now.

---
+++ server/lib/arbpl/mozmill-logfrob.js
@@ -0,0 +1,122 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at:
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozilla Messaging Code.
+ *
+ * The Initial Developer of the Original Code is
+ *   The Mozilla Foundation
+ * Portions created by the Initial Developer are Copyright (C) 2011
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Andrew Sutherland <asutherland@asutherland.org>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+
+var RE_START = /^##### MOZMILL-RICH-FAILURES-BEGIN #####$/m;
+var RE_END = /^##### MOZMILL-RICH-FAILURES-END #####$/m;
+var OVERLAP_PADDING = 16;
+
+/**
+ * Consume
+ */
+function Frobber(stream, callback) {
+  stream.on("data", this.onData.bind(this));
+  stream.on("end", this.onEnd.bind(this));
+
+  this.callback = callback;
+
+  this.jsonObjs = [];
+  this.inBlock = false;
+  this.leftover = null;
+}
+Frobber.prototype = {
+  _gobbleJsonFromLines: function(dstr) {
+    var bits = dstr.split("\n");
+    for (var i = 0; i < bits.length; i++) {
+      this.jsonObjs.push(JSON.parse(bits[i]));
+    }
+  },
+  onData: function(data) {
+    var dstr = this.leftover ?
+                 (this.leftover + data.toString("utf8")) :
+                 data.toString("utf8");
+    this.leftover = null;
+
+    var match;
+    while (dstr) {
+      if (this.inBlock) {
+        match = RE_END.exec(dstr);
+        if (match) {
+          this._gobbleJsonFromLines(dstr.substring(0, match.index - 1));
+          dstr = dstr.substring(match.index + match[0].length + 1);
+          this.inBlock = false;
+          continue;
+        }
+        else {
+          var lastNewline = dstr.lastIndexOf("\n");
+          if (lastNewline != -1) {
+            this._gobbleJsonFromLines(dstr.substring(0, lastNewline));
+            this.leftover = dstr.substring(lastNewline + 1);
+          }
+          else {
+            this.leftover = dstr;
+          }
+          break;
+        }
+      }
+      else {
+        match = RE_START.exec(dstr);
+        if (match) {
+          // gobble up after the newline
+          dstr = dstr.substring(match.index + match[0].length + 1);
+          this.inBlock = true;
+          continue;
+        }
+        else {
+          // nothing interesting in here; but potentially keep some overlap
+          var maybeLeftover = dstr.slice(-OVERLAP_PADDING);
+          if (maybeLeftover.indexOf("#") != -1)
+            this.leftover = maybeLeftover;
+          break;
+        }
+      }
+    }
+  },
+
+  onEnd: function(data) {
+    this.callback(this.jsonObjs);
+  },
+};
+
+var $fs = require("fs");
+var stream = $fs.createReadStream("/tmp/mozmill-log");
+
+var frobber = new Frobber(stream, function(objs) {
+  for (var i = 0; i < objs.length; i++) {
+    console.log(objs[i].exception.message);
+  }
+});
---
+++ server/lib/sync-hello.js
@@ -0,0 +1,81 @@
+var $thrift =  require("thrift");
+var $thriftHbase = require("./hbase-thrift/Hbase");
+var $baseTypes = require("./hbase-thrift/Hbase_types");
+
+var connection = $thrift.createConnection("localhost", 9090);
+var client = $thrift.createClient($thriftHbase, connection);
+
+connection.on("connect", function() {
+  console.log("connected!");
+});
+connection.on("error", function(err) {
+  console.error(err);
+});
+
+client.isTableEnabled("hello", function(error, response) {
+  console.log("error", error, "response", response);
+});
+
+var helloColumnDefs = [
+  new $baseTypes.ColumnDescriptor({
+    name: "helloTo",
+  }),
+];
+
+function makeTable() {
+  client.createTable(
+    "hello",
+    helloColumnDefs,
+    function(error) {
+      console.log("table creation", error);
+      addRow();
+    }
+  );
+}
+
+function addRow() {
+  client.mutateRow(
+    "hello",
+    "bob",
+    [
+      new $baseTypes.Mutation({
+        column: "helloTo:world",
+        value: "yo!",
+      }),
+      new $baseTypes.Mutation({
+        column: "helloTo:moon",
+        value: "wassup?",
+      }),
+    ],
+    function(error) {
+      console.log("row updation", error);
+      getRow();
+    }
+  );
+}
+
+function getRow() {
+  client.getRow(
+    "hello",
+    "bob",
+    function(error, rowResults) {
+      console.log("getRow", error);
+      if (rowResults)
+        dumpRowResults(rowResults);
+    }
+  );
+}
+
+function dumpRowResults(rowResults) {
+  for (var i = 0; i < rowResults.length; i++) {
+    var rr = rowResults[i];
+    console.log("row:", rr.row);
+    var cols = rr.columns;
+    for (var key in cols) {
+      var cell = cols[key];
+      console.log("  key:", key, "value", cell.value, "ts", cell.timestamp);
+    }
+  }
+}
+
+makeTable();
