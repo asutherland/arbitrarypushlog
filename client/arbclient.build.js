9fb6c7cc6fc553a0b6dc707f54977ea22e50c4f7
Andrew Sutherland <asutherland@asutherland.org>
2011-03-03 15:54:19 -0800

    add scripts to ease running, baby steps to requirejs optimization.

---
+++ client/arbclient.build.js
@@ -0,0 +1,17 @@
+({
+  baseUrl: "./",
+  dir: "build",
+  optimize: "none",
+  packages: [
+    { name: "arbclient", location: ".", lib: "lib/arbclient" },
+    { name: "arbcommon", location: ".", lib: "lib/arbcommon" },
+    // jstut family
+    { name: "jstut", location: "deps/jtstut",
+      lib: "lib/jstut" },
+    { name: "narscribblus", location: "deps/jstut",
+      lib: "lib/narscribblus" },
+    { name: "narscribblus-plat", location: "deps/jstut",
+      lib: "lib-requirejs/narscribblus-plat" },
+    { name: "wmsy", location: "deps/wmsy", lib: "lib/wmsy" },
+  ]
+})
