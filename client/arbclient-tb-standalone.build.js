071f6779edd61b3b4ac2d6c97159076b6d307db5
Andrew Sutherland <asutherland@asutherland.org>
2012-02-07 16:45:39 -0800

    standalone TB build works!

---
+++ client/arbclient-tb-standalone.build.js
@@ -0,0 +1,24 @@
+({
+  baseUrl: "./",
+  optimize: "none",
+  paths: {
+    "text": "deps/requirejs/text",
+    "d3": "deps/d3.min",
+    "requireLib": "deps/requirejs/require",
+    "socket.io/socket.io": "deps/fake-socket-io",
+  },
+      packages: [
+        { name: "arbclient", location: "lib/arbclient" },
+        { name: "arbcommon", location: "lib/arbcommon" },
+
+        // jstut family
+        { name: "jstut", location: "deps/jtstut/lib/jstut" },
+        { name: "narscribblus", location: "deps/jstut/lib/narscribblus" },
+        { name: "narscribblus-plat",
+          location: "deps/jstut/lib-requirejs/narscribblus-plat" },
+        { name: "wmsy", location: "deps/wmsy/lib/wmsy" },
+      ],
+  include: ["requireLib"],
+  name: "arbclient/app-tb-mozmill",
+  out: "standalone-tb-arbclient.js"
+})
